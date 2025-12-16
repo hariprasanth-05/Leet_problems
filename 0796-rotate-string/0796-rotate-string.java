196bff43a42266e822bf333ff7f23a7010580846
Hariprasanth P <ariprasanthpalani@gmail.com>
2025-12-16 22:09:39 +0530

    Time: 1 ms (24.75%), Space: 43.3 MB (6.06%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -4,7 +4,7 @@ class Solution {
         int goalLen = goal.length();
         if(sLen != goalLen) return false;
         for(int i=0; i<sLen; i++){
-            String str = s.substring(sLen-i-1)+s.substring(0,sLen - i-1);
+            String str = s.substring(i)+s.substring(0,i);
             if(str.equals(goal)){
                 return true;
             }
