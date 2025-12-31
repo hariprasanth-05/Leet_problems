20d0fe6179cb4c670a2da2dd32fcb0907ff9c556
Hariprasanth P <ariprasanthpalani@gmail.com>
2025-12-31 18:33:26 +0530

    Time: 5 ms (97.21%), Space: 47.1 MB (21.16%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -6,7 +6,9 @@ class Solution {
         }
 
         for(int i=0; i< s.length(); i++){
-            if(hasharr[s.charAt(i)-'a'] == 1) return i;
+            if(hasharr[s.charAt(i)-'a'] == 1){
+                return i;
+            } 
         }
         return -1;
     }
