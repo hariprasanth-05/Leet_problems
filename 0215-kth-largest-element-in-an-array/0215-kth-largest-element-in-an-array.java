2b7c53ccc0bd63d8215fafddc73fef97993e72f1
Hariprasanth P <ariprasanthpalani@gmail.com>
2026-01-05 22:24:02 +0530

    Time: 2210 ms (5.01%), Space: 74.6 MB (5.07%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,39 +1,48 @@
 class Solution {
-    public int quickSort(int[] arr, int low, int high,int target) {
 
-        if(low <= high){
-             int pivotInd = partition(arr,low,high);
-             if(pivotInd == target) return arr[pivotInd];
-             else if (pivotInd > target) {
-                return quickSort(arr, low, pivotInd - 1, target);
+    public int quickSelect(int[] arr, int low, int high, int target) {
+
+        if (low <= high) {
+            int pivotInd = partition(arr, low, high);
+
+            if (pivotInd == target) {
+                return arr[pivotInd];
+            }
+            else if (pivotInd > target) {
+                return quickSelect(arr, low, pivotInd - 1, target);
             }
             else {
-                return quickSort(arr, pivotInd + 1, high, target);
+                return quickSelect(arr, pivotInd + 1, high, target);
             }
         }
-        return-1;
-        
+        return -1; // safety return
     }
+
     private int partition(int[] arr, int low, int high) {
         int pivot = arr[low];
         int i = low;
         int j = high;
-        while(i < j){
-            while(arr[i] <= pivot && i <= high - 1) i++;
-            while(arr[j] > pivot && j >= low + 1) j--;
-            if(i < j){
+
+        while (i < j) {
+            while (i <= high - 1 && arr[i] <= pivot) i++;
+            while (j >= low + 1 && arr[j] > pivot) j--;
+
+            if (i < j) {
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
+
         int temp = arr[low];
         arr[low] = arr[j];
-        arr[j] = temp; 
+        arr[j] = temp;
+
         return j;
     }
+
     public int findKthLargest(int[] nums, int k) {
-        int target = nums.length-k;
-        return quickSort(nums,0,nums.length-1,target);
+        int target = nums.length - k; // kth largest → index
+        return quickSelect(nums, 0, nums.length - 1, target);
     }
 }
