d302c24dca8f682bd0d83a2d9f778416248f7cd0
Hariprasanth P <ariprasanthpalani@gmail.com>
2026-01-25 20:00:03 +0530

    Time: 9 ms (26.64%), Space: 47 MB (43.99%) - LeetHub

---
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        if(k == 1) return 0;
+        Arrays.sort(nums);
+        int left = 0;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i+k-1 < nums.length; i++){
+            int diff = nums[i+k-1] - nums[i];
+            min = Math.min(min,diff);
+        }
+        return min;
+    }
+}
