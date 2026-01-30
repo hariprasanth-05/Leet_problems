88f4c481a6176fe5bfa64af9b02639e8a49d23ea
Hariprasanth P <ariprasanthpalani@gmail.com>
2026-01-31 00:43:15 +0530

    Time: 56 ms (30.54%), Space: 68.9 MB (77.63%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -7,7 +7,7 @@ class Solution {
         while(r < fruits.length){
             freq.put(fruits[r], freq.getOrDefault(fruits[r],0)+1);
 
-            while(freq.size() > 2){
+            if(freq.size() > 2){
                 freq.put(fruits[l], freq.get(fruits[l])-1);
                 if(freq.get(fruits[l]) == 0){
                     freq.remove(fruits[l]);
