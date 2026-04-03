a730af977f7d48a8f8c83ce46f3fd3d608b0beb2
Hariprasanth P <ariprasanthpalani@gmail.com>
2026-04-03 20:46:53 +0530

    Time: 2 ms (100%), Space: 46.6 MB (44.14%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,16 +1,30 @@
 class Solution {
     public int[] minOperations(String boxes) {
         int n = boxes.length();
-        int[] res = new int[n]; 
+        int[] arr = new int[n];
         for(int i = 0; i < n; i++){
-            int j = (i+1)%n;
-            while(i != j){
-                if(boxes.charAt(j) == '1'){
-                    res[i] += Math.abs(j - i);
-                }
-                j = (j+1) % n;
-            }
+            arr[i] = boxes.charAt(i)-'0';
         }
-        return res;
+
+        int left[] = new int[n];
+        int right[] = new int[n];
+
+        int cnt = arr[0];
+        for(int i = 1; i < n; i++){
+            left[i] = left[i-1] + cnt;
+            cnt += arr[i];
+        }
+
+        cnt = arr[n-1];
+        for(int i = n-2; i >= 0; i--){
+            right[i] = right[i+1] + cnt;
+            cnt += arr[i];
+        }
+
+        // -+int[] res = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = left[i]+right[i];
+        }
+        return arr;
     }
 }
